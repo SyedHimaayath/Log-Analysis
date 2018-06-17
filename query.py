b27253f68b02c89bdc8dfff3d1dec466eee7b61f
Syed Himaayath <syedhimaayath155@gmail.com>
2018-06-18 00:07:38 +0530

    pep8 conventions

--- query.py
+++ query.py
@@ -14,7 +14,7 @@ def take_query(query):
 def popular_articles():
     query_01 = '''select title, views from pop_articles limit 3;'''
     results = take_query(query_01)
-    print("01. What are the most popular three articles of all time?")
+    print("01.  What are the most popular three articles of all time?")
     for name, num in results:
         print("sol: " + name + " - " + str(num) + " views")
         print("\n\n")
@@ -25,7 +25,7 @@ def popular_authors():
     views from pop_articles, authors where pop_articles.author = author.id
     group by authors.name order by views desc;'''
     results = take_query(query_02)
-    print("01. Who are the most popular article author of all time?")
+    print("02.  Who are the most popular article author of all time?")
     for name, num in results:
         print("sol: " + name + " - " + str(num) + " views")
     print("\n\n")
@@ -35,7 +35,7 @@ def error_log():
     query_03 = '''select to_char(date,'MON DD YYYY') as date, percent_error
     from log_err_view where percent_error > 1;'''
     results = take_query(query_03)
-    print("01. What are the most popular three articles of all time?")
+    print("03.  What are the most popular three articles of all time?")
     for date, value in results:
         print("sol: " + date + " - " + str(value) + " %"+" errors")
     print("\n\n")
